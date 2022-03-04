467d4f9cb23af00de31198a57277d7f9d8226ab6
iigunchev <iigunchev@gmail.com>
2022-03-04 15:25:24 +0100

    routing

--- server/index.js
+++ server/index.js
@@ -3,18 +3,20 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js"
+
 
 const app = express();
+
+app.use("/posts", postRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL = "mongodb+srv://ivan:Password1@cluster0.7qazo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch(() => console.log(error.message));
-
-mongoose.set("useFindAndModify", false);
+    .catch((error) => console.log(error.message));
