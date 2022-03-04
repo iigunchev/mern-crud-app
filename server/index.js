32c8cb5447d430df647167687193c608adc10a30
iigunchev <iigunchev@gmail.com>
2022-03-04 15:09:46 +0100

    connecting to the database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://ivan:Password1@cluster0.7qazo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch(() => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
