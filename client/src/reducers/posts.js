750e9f359183f793981b7c0da41e6e239d0ab730
iigunchev <iigunchev@gmail.com>
2022-03-07 22:43:50 +0100

    like post implemented

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -7,6 +7,7 @@ const dispatch = (posts = [], action) => {
         case "CREATE":
             return [ ...posts, action.payload];
         case "UPDATE":
+        case "LIKE":
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         default:
             return posts;
