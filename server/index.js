38eb903a055e33520bc36d891f353b2d50d614a8
iigunchev <iigunchev@gmail.com>
2022-03-09 17:28:17 +0100

    update

--- server/index.js
+++ server/index.js
@@ -2,20 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js"
 
 
 const app = express();
+dotenv.config();
 
-app.use("/posts", postRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://ivan:Password1@cluster0.7qazo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 3000;
+app.use("/posts", postRoutes);
+
+// app.get("/", (req, res => {
+//     res.send("MERN CRUD App")
+// }));
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
