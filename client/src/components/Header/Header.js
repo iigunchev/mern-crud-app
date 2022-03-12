fa659b80ad81a4c718feaa7b5f29f82d97f29368
iigunchev <iigunchev@gmail.com>
2022-03-12 19:21:55 +0100

    created routing and modified header for auth

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,20 +1,32 @@
-export default function Header({}) {
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    const user = null;
+
     return (
     <header className="
-    mt-4 
-    bg-slate-800 
-    p-4 
-    rounded-lg 
-    flex 
-    justify-between 
-    items-center 
-    md:mx-auto 
-    md:container">
-      <h3 className="text-slate-200 text-xl font-bold">6 Suggestions</h3>
-      <div>
-        <span className="text-slate-200 sm:block hidden">Sort by: Most Upvotes</span>
+        mt-4 
+        bg-slate-800 
+        p-4 
+        rounded-lg 
+        flex 
+        justify-between 
+        items-center 
+        md:mx-auto 
+        md:container">
+    <Link to="/" className="text-slate-200 text-xl font-bold" >Home</Link>
 
-      </div>
-      <button className="bg-fuchsia-700 text-white py-3 px-6 font-semibold rounded-lg hover:bg-fuchsia-600 active:bg-fuchsia-700 focus:outline-none focus:ring focus:ring-fuchsia-300">+ Add Feedback</button>
-    </header>);
+    <button className="bg-fuchsia-700 text-white py-3 px-6 font-semibold rounded-lg hover:bg-fuchsia-600 active:bg-fuchsia-700 focus:outline-none focus:ring focus:ring-fuchsia-300">+ Add Feedback</button>
+    {user ? (
+        <div>
+            <img src={user.result.imageUrl} alt={user.result.name}>{user.result.name.charAt(0)}</img>
+            <h6>{user.result.name}</h6>
+            <button>Log Out</button>
+        </div>
+    ) : (
+        <Link to="/auth" className="bg-slate-300 rounded-lg p-2 font-medium">Log In</Link>
+    )}
+    </header>
+    );
   }
